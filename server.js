6013f6c93156a03d503b36e2cf300777dc7c4545
JohnAntonusMaximus <johnradosta@gmail.com>
2016-06-16 17:41:46 -0500

    Added timestamp functionality

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,11 +13,14 @@ io.on('connection',function(socket){
     
     socket.on('message', function(message){
         console.log('Message Received: ' + message.text);
+        
+        message.timestamp = moment().valueOf();
         io.emit('message', message);
     });
     
     socket.emit('message', {
-        text: 'Welcome to Node Chat!'
+        text: 'Welcome to Node Chat!',
+        timestamp: moment.valueOf()
     });
 });
 
