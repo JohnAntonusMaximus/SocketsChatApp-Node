ceec9b1a7da12e703a534a245cea6e705272f8f0
JohnAntonusMaximus <johnradosta@gmail.com>
2016-06-23 11:53:43 -0500

    Removed console.logs

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,6 @@ var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 var socket = io();
 
-console.log(name + ' wants to join ' + room);
-
 // Update h1 tag
 jQuery('.room-title').text(room);
 
@@ -19,11 +17,6 @@ socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	var $messages = jQuery('.messages');
 	var $message = jQuery('<li class="list-group-item"></li>');
-
-	console.log('New message:');
-	console.log(message.text);
-	
-	
 
 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
